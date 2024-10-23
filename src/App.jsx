9bc794f2a155ee9720ad5e02d8ed5544d0f47242
podusit <d.rimdusit@gmail.com>
2024-10-23 14:15:34 +0700

    feat:greeting message box with form handling ex-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingInputText, setgreetingInputText] = useState("");
+  const [greetingText, setGreetingText] = useState("Greeting Message");
+
+  const updateGreetingInputText = (event) => {
+    setgreetingInputText(event.target.value);
+  };
+  const updateGreetingText = () => {
+    setGreetingText(greetingInputText);
+  };
+
   return (
+    <>
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{" "}{greetingText}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" value={greetingInputText} onChange={updateGreetingInputText} />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={updateGreetingText}>Update text</button>
       </div>
     </div>
+    </>
   );
 }
 
